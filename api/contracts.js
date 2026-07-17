5376c7be18e8e284904b5d21074021155e9efccb
bn-ge <bnavarrete@ganaenergia.com>
2026-07-17 14:48:11 +0200

    Update contracts.js

--- api/contracts.js
+++ api/contracts.js
@@ -8,6 +8,15 @@
 const data = require("../data/contracts.json");
 
 module.exports = async (req, res) => {
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader("Access-Control-Allow-Methods", "GET, OPTIONS");
+  res.setHeader("Access-Control-Allow-Headers", "Content-Type");
+
+  if (req.method === "OPTIONS") {
+    res.status(200).end();
+    return;
+  }
+
   const { simulateError, delay } = req.query;
 
   const waitMs = delay ? parseInt(delay, 10) : 400;
